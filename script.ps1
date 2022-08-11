74773f2cbee3a93eaf301226fe45801a6ee1101f
rubenv11003 <rubenv11003@yahoo.com>
2022-08-11 16:12:49 +0100

    Added initial project plan

--- script.ps1
+++ script.ps1
@@ -2,3 +2,4 @@ Initial content for script.ps1
 Temporary commit content for 2022-07-03T16:12:49
 Temporary commit content for 2022-07-17T16:12:49
 Temporary commit content for 2022-07-31T16:12:49
+Temporary commit content for 2022-08-11T16:12:49
