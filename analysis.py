d9b2f4f6211610a09bfb67731875db9894eac572
rubenv11003 <rubenv11003@yahoo.com>
2022-08-21 16:12:49 +0100

    Updated README with new features

--- analysis.py
+++ analysis.py
@@ -3,3 +3,4 @@ Temporary commit content for 2022-06-30T16:12:49
 Temporary commit content for 2022-07-06T16:12:49
 Temporary commit content for 2022-07-16T16:12:49
 Temporary commit content for 2022-08-02T16:12:49
+Temporary commit content for 2022-08-21T16:12:49
