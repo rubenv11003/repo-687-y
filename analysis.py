3a52a72eb649ece09ee486c5f0b7b456ca671c4a
rubenv11003 <rubenv11003@yahoo.com>
2022-09-06 16:12:49 +0100

    Fixed typos in documentation

--- analysis.py
+++ analysis.py
@@ -4,3 +4,4 @@ Temporary commit content for 2022-07-06T16:12:49
 Temporary commit content for 2022-07-16T16:12:49
 Temporary commit content for 2022-08-02T16:12:49
 Temporary commit content for 2022-08-21T16:12:49
+Temporary commit content for 2022-09-06T16:12:49
