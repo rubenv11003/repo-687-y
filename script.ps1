3cf414944f04737aefac6134a93191e6a6411ef4
rubenv11003 <rubenv11003@yahoo.com>
2023-01-17 16:12:49 +0100

    Refactored code for better readability

--- script.ps1
+++ script.ps1
@@ -18,3 +18,4 @@ Temporary commit content for 2022-12-09T16:12:49
 Temporary commit content for 2022-12-13T16:12:49
 Temporary commit content for 2023-01-08T16:12:49
 Temporary commit content for 2023-01-16T16:12:49
+Temporary commit content for 2023-01-17T16:12:49
