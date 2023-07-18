d5869f66118a8641da54a77ad7f25d6ff3e9654a
rubenv11003 <rubenv11003@yahoo.com>
2023-07-18 16:12:49 +0100

    Fixed bug in data processing script

--- script.ps1
+++ script.ps1
@@ -32,3 +32,4 @@ Temporary commit content for 2023-05-11T16:12:49
 Temporary commit content for 2023-05-13T16:12:49
 Temporary commit content for 2023-05-31T16:12:49
 Temporary commit content for 2023-07-14T16:12:49
+Temporary commit content for 2023-07-18T16:12:49
