4639651b4c3b21a03abe898c2e122ba62325a01a
rubenv11003 <rubenv11003@yahoo.com>
2023-08-22 16:12:49 +0100

    Updated README with new features

--- script.ps1
+++ script.ps1
@@ -35,3 +35,4 @@ Temporary commit content for 2023-07-14T16:12:49
 Temporary commit content for 2023-07-18T16:12:49
 Temporary commit content for 2023-08-17T16:12:49
 Temporary commit content for 2023-08-19T16:12:49
+Temporary commit content for 2023-08-22T16:12:49
