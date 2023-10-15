e71d6921ec94803c6da57a5ff4e67b3afe33e2ad
rubenv11003 <rubenv11003@yahoo.com>
2023-10-15 16:12:49 +0100

    Added validation for user input

--- script.ps1
+++ script.ps1
@@ -39,3 +39,4 @@ Temporary commit content for 2023-08-22T16:12:49
 Temporary commit content for 2023-09-19T16:12:49
 Temporary commit content for 2023-10-06T16:12:49
 Temporary commit content for 2023-10-12T16:12:49
+Temporary commit content for 2023-10-15T16:12:49
