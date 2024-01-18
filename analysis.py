eaad8c9a3f4da2067961055861d4bcfb13228e71
rubenv11003 <rubenv11003@yahoo.com>
2024-01-18 16:12:49 +0100

    Added validation for user input

--- analysis.py
+++ analysis.py
@@ -48,3 +48,4 @@ Temporary commit content for 2023-12-14T16:12:49
 Temporary commit content for 2023-12-26T16:12:49
 Temporary commit content for 2023-12-31T16:12:49
 Temporary commit content for 2024-01-10T16:12:49
+Temporary commit content for 2024-01-18T16:12:49
